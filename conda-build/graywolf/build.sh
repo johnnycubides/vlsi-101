ef544fb53e095da9aa39eb61b41bbd75a6297889
Johnny Cubides <jgcubidesc@gmail.com>
2023-12-09 13:23:14 -0500

    ok graywolf build

--- conda-build/graywolf/build.sh
+++ conda-build/graywolf/build.sh
@@ -12,7 +12,17 @@ status=$?
 # cd netgen/
 mkdir build
 cd build
-cmake ..
+cmake -DCMAKE_C_FLAGS=-fcommon -DCMAKE_INSTALL_PREFIX:PATH=$CONDA_PREFIX ..
 # --prefix=$CONDA_PREFIX
 make -j$(nproc)
 make install
+
+
+# https://molcasforum.univie.ac.at/viewtopic.php?id=714
+# I am trying to compile Luscus 0.8.6 from source in Arch Linux.
+# My CMake version is 3.19.3, and I am using GNU GCC compilers 10.2.0. I installed all of the required libraries through the pacman package manager.
+
+# Found the solution.
+
+# The default "-fcommon" flag changed to "-fno-common" in GCC 10.
+# This problem is fixed by adding -DCMAKE_C_FLAGS=-fcommon when running cmake.
